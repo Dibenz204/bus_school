a129bd0adae1e402ec1672d75705beaef94753db
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2025-10-26 20:10:47 +0700

    home_login_user

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { Toaster } from "sonner";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+// function App() {
+//   return (
+//     <>
+//       <Toaster richColors />
+
+//       <BrowserRouter>
+//         <Routes>
+// <Route path="/trangchu" element={<Home_TrangChu />} />
+// <Route path="/" element={<Introduce />} />
+// <Route path="/login" element={<LoginPage />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster, toast } from "sonner";
+import Test from "./pages/Test";
+import Parent from "./pages/Parent";
+import Home_TrangChu from "./pages/Home_TrangChu";
+import LoginPage from "./pages/LoginPage";
+import Introduce from "./pages/Introduce";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/trangchu" element={<Home_TrangChu />} />
+          <Route path="/" element={<Introduce />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/test_parent" element={<Parent />} />
+
+        </Routes>
+      </BrowserRouter>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
